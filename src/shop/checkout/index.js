2e22692325e6f84a0a7517fd39462e81554cd91e
Jon Shipman <jon@jonshipman.com>
2020-12-29 09:44:50 -0600

    replacement

--- src/shop/checkout/index.js
+++ src/shop/checkout/index.js
@@ -3,5 +3,6 @@ export * from "./CheckoutPage";
 export * from "./ExternalPaymentMethod";
 export * from "./PaymentMethod";
 export * from "./ShippingMethod";
+export * from "./States";
 export * from "./useCheckout";
 export * from "./useMethods";
